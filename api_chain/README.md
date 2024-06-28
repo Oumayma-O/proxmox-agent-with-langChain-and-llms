7e134efc22d49efc3ae1f8588e68efaa2b8bd398
Jawher <j.ressaissi@coral-io.fr>
2024-06-28 15:52:48 +0100

    Add api chain

---
+++ api_chain/README.md
@@ -0,0 +1,30 @@
+# API chain
+
+## Description
+
+This project provides the PowerfulAPIChain class which subclasses Langchain's APIChain to provide support for more HTTP methods.
+
+## Getting started
+
+Steps to get started with the project assuming you have Python 3 installed:
+
+```bash
+git clone https://gitlab.coralio.fr/infrastructure-management-x/generative-ai/agent-scripts.git
+cd agent-scripts/api_chain
+python -m venv env
+env\Scripts\Activate
+pip install -r requirements.txt
+```
+
+## Roadmap
+
+* [X] Test HTTP GET request
+* [X] Test HTTP POST request
+* [ ] Test HTTP PUT request
+* [ ] Test HTTP DELETE request
+* [ ] Test HTTP PATCH request
+* [ ] Implement HTTP HEAD
+* [ ] Implement async support
+* [ ] Optimize API docs formatting
+* [ ] Optimize default prompt templates
+* [ ] Document most performing prompting strategies
---
+++ api_chain/core/powerfulchain.py
@@ -0,0 +1,77 @@
+import json
+
+from core.templates import API_REQUEST_PROMPT, API_RESPONSE_PROMPT
+from langchain.chains import APIChain
+from typing import Any, Dict, Optional
+from langchain.prompts import BasePromptTemplate
+from langchain.chains.llm import LLMChain
+from langchain_community.utilities.requests import TextRequestsWrapper
+from langchain_core.language_models import BaseLanguageModel
+from langchain_core.callbacks import (
+    CallbackManagerForChainRun,
+)
+
+
+class PowerfulAPIChain(APIChain):
+    def _call(self,
+              inputs: Dict[str, str],
+              run_manager: Optional[CallbackManagerForChainRun] = None) -> Dict[str, str]:
+        _run_manager = run_manager or CallbackManagerForChainRun.get_noop_manager()
+        question = inputs[self.question_key]
+        request_info = self.api_request_chain.predict(
+            question=question, api_docs=self.api_docs, callbacks=_run_manager.get_child()
+        )
+        if self.verbose:
+            print(f'Request info: {request_info}')
+
+        try:
+            api_url, request_method, body = request_info.split('|', 2)
+        except Exception as e:
+            return {self.output_key: "", "error": f"Output parse error: {str(e)}"}
+        
+        api_url, request_method, body = api_url.strip().replace('|', ''), request_method.strip().replace('|', ''), body.strip().replace('|', '')
+
+        if self.verbose:
+            print(f"\nAPI URL: {api_url}")
+            print(f"\nRequest method: {request_method}")
+            print(f"\nRequest body: {body}")
+
+        # Resolve the method name
+        request_method_lower = request_method.lower()
+        request_func = getattr(self.requests_wrapper, request_method_lower)
+
+        if request_method_lower in ["get", "delete", "head"]:
+            api_response = request_func(api_url)
+        else:
+            api_response = request_func(api_url, json.loads(body))
+
+        answer = self.api_answer_chain.predict(
+            question=question,
+            api_docs=self.api_docs,
+            api_url=api_url,
+            api_response=api_response,
+            callbacks=_run_manager.get_child()
+        )
+        return {self.output_key: answer}
+
+    @classmethod
+    def from_llm_and_api_docs(
+        cls,
+        llm: BaseLanguageModel,
+        api_docs: str,
+        headers: Optional[dict] = None,
+        api_url_prompt: BasePromptTemplate = API_REQUEST_PROMPT,
+        api_response_prompt: BasePromptTemplate = API_RESPONSE_PROMPT,
+        **kwargs: Any,
+    ) -> APIChain:
+        """Load chain from just an LLM and the api docs."""
+        get_request_chain = LLMChain(llm=llm, prompt=api_url_prompt)
+        requests_wrapper = TextRequestsWrapper(headers=headers)
+        get_answer_chain = LLMChain(llm=llm, prompt=api_response_prompt)
+        return cls(
+            api_request_chain=get_request_chain,
+            api_answer_chain=get_answer_chain,
+            requests_wrapper=requests_wrapper,
+            api_docs=api_docs,
+            **kwargs,
+        )
