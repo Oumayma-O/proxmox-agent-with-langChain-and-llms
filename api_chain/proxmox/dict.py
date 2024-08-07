c973ca284d62b5e4d280fa154c5f846998113eaf
Oumayma-O <oumaymaouerfelli3@gmail.com>
2024-08-07 15:31:24 +0100

    proxmox subgraph

--- api_chain/proxmox/dict.py
+++ api_chain/proxmox/dict.py
@@ -10,11 +10,11 @@ node5_api_token = os.getenv('NODE_5_API_TOKEN')
 
 # Define the Proxmox nodes with their base URLs and API tokens
 proxmox_nodes = {
-    "proxmoxHCM": {
+    "ProxmoxHCM": {
         "base_url": "https://ns31418912.ip-54-38-37.eu:8006",
         "api_token": proxmox_HCM_api_token
     },
-    "node5": {
+    "Node5": {
         "base_url": "https://ns31212248.ip-51-178-74.eu:8006",
         "api_token": node5_api_token
     }
