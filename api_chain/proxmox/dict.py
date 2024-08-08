0efbad69bec3415bbb8c6e73cdb6bddbf29dd791
Oumayma-O <oumaymaouerfelli3@gmail.com>
2024-08-08 12:05:15 +0100

    workers name in sup prompt became dynamic and dict keys changed

--- api_chain/proxmox/dict.py
+++ api_chain/proxmox/dict.py
@@ -10,11 +10,11 @@ node5_api_token = os.getenv('NODE_5_API_TOKEN')
 
 # Define the Proxmox nodes with their base URLs and API tokens
 proxmox_nodes = {
-    "ProxmoxHCM": {
+    "Proxmox-Node-HCM": {
         "base_url": "https://ns31418912.ip-54-38-37.eu:8006",
         "api_token": proxmox_HCM_api_token
     },
-    "Node5": {
+    "ns31212248": {
         "base_url": "https://ns31212248.ip-51-178-74.eu:8006",
         "api_token": node5_api_token
     }
